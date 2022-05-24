17db4100d7ece330dc45bb78144c38966bebd589
katy01101011 <katy.91560@gmail.com>
2022-05-24 17:14:45 +0200

    add: stars

--- src/components/BoolflixCard.vue
+++ src/components/BoolflixCard.vue
@@ -1,19 +1,27 @@
 <template>
   <section>
-    <div v-if="film">
+    <div class="card" v-if="film">
       <img :src="`https://image.tmdb.org/t/p/w200${this.film.poster_path}`" alt="">
       <h2>{{ this.film.title }}</h2>
       <h3>{{ this.film.original_title }}</h3>
+      
       <country-flag :country="`${this.film.original_language}`" />
-      <p>{{ Math.round( this.film.vote_average / 2 ) }}</p>
+      <div>
+        <i v-for="star in stars(this.film.vote_average)" :key="star" class="fa-solid fa-star"></i>
+      </div>
+      
     </div>
 
-    <div v-if="serie">
+
+    <div class="card" v-if="serie">
       <img :src="`https://image.tmdb.org/t/p/w200${this.serie.poster_path}`" alt="">
       <h2>{{ this.serie.name }}</h2>
       <h3>{{ this.serie.original_title }}</h3>
       <country-flag :country="`${this.serie.original_language}`" />
-      <p>{{ this.serie.vote_average }}</p>
+      <div>
+        <i v-for="star in stars(this.serie.vote_average)" :key="star" class="fa-solid fa-star"></i>
+      </div>
+      <p>{{ this.seriesStars }}</p>
     </div>
   </section>
 </template>
@@ -23,10 +31,18 @@ import CountryFlag from "vue-country-flag";
 
 export default {
   name: "BoolflixCard",
+  
   props: {
     film: Object,
     serie: Object,
   },
+  
+  methods: {
+    stars(vote) {
+      return Math.ceil(vote / 2);
+    }
+  },
+
   components: {
     CountryFlag,
   },
@@ -36,7 +52,8 @@ export default {
 <style lang="scss" scoped>
 @import "~@fortawesome/fontawesome-free/css/all.min.css";
 div {
-  div {
+  .card {
+    width: 200px;
     border: 1px solid black;
     margin: 1rem;
   }
